f11d432dc76238f59b1f83f559b15fe0c0469924
Tacitor <lukasjkrampitz@gmail.com>
2026-03-17 14:14:33 +0000

    Start the constructor for the GameUI

--- python/ui/game_ui.py
+++ python/ui/game_ui.py
@@ -1,1 +1,8 @@
 #GameUI should query the GameEngine for state and call rendering functions only.
+
+class GameUI:
+    def __init__(self, config_path: str, profile_path: str):
+        self._config = config_path
+        self._profile = profile_path
+        
+        print("UI Time: " + self._profile + ", " + self._config)
