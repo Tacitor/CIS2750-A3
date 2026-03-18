2e81cfe3014607d4d7d7e50b875639feec9879f1
Tacitor <lukasjkrampitz@gmail.com>
2026-03-18 22:33:26 +0000

    Make the response to different terminal sizes a bit more graceful

--- python/treasure_runner/ui/game_ui.py
+++ python/treasure_runner/ui/game_ui.py
@@ -11,14 +11,13 @@ class GameUI:
         self._eng = GameEngine(self._config)
 
         self._message_bar = "Start by moving around"
-        self._lowest_str = 0
-        self._rightest_str = 0
 
     def launch(self):
         print("Launching the TUI...")
         try:
             curses.wrapper(self._run_tui)
         except BufferError as exc:
+            # TODOFIXME: Is this what they mean by raise an error? Should I be catching it again?
             print("[ERROR] Sorry it looks like your terminal is too small to display the game.")
             print(str(exc))
         print("Done game and done with the GameEngine")
@@ -41,37 +40,37 @@ class GameUI:
         self._splash_player_info(stdscr)
 
     def _ui_main_game(self, stdscr):
-        game_element_offset = 30
-
         user_input = ord('z')
         while user_input != ord('q') and user_input != ord('Q'):
-            # TODOFIXME: Update this
-            self._lowest_str = 23
-            # TODOFIXME: Update this
-            self._rightest_str = game_element_offset
+            room_row = 3
+            room_col = 0
+            room_width, room_height = self._eng.get_room_dimensions()
+            game_element_width_offset = room_width + 5
+            game_element_height_offset = room_height + room_row + 1
 
             stdscr.clear()
             safe_addstr(stdscr, 0, 0, self._message_bar)
             safe_addstr(stdscr, 1, 0, "<<room number and name here>>")
 
             room_str = self._eng.render_current_room()
-            room_row = 3
-            room_col = 0
             player_x, player_y = self._eng.player.get_position()
             safe_addstr(stdscr, room_row, room_col, room_str)
             # TODOFIXME: Make safe?
             stdscr.addch(room_row + player_y, room_col + player_x, "@", curses.color_pair(2))
 
-            safe_addstr(stdscr, 3, game_element_offset, "Game Elements:")
-            safe_addstr(stdscr, 5, game_element_offset, "@ - Julia")
-            safe_addstr(stdscr, 6, game_element_offset, "# - wall")
-            safe_addstr(stdscr, 7, game_element_offset, "$ - gold")
-            safe_addstr(stdscr, 8, game_element_offset, "X - portal")
-            safe_addstr(stdscr, 9, game_element_offset, "<<other elements/names here>>")
+            side_bar_lowest = 9
+            safe_addstr(stdscr, 3, game_element_width_offset, "Game Elements:")
+            safe_addstr(stdscr, 5, game_element_width_offset, "@ - Julia")
+            safe_addstr(stdscr, 6, game_element_width_offset, "# - wall")
+            safe_addstr(stdscr, 7, game_element_width_offset, "$ - gold")
+            safe_addstr(stdscr, 8, game_element_width_offset, "X - portal")
+            safe_addstr(stdscr, side_bar_lowest, game_element_width_offset, "<<other elements/names here>>")
 
-            safe_addstr(stdscr, 20, 0, "Game Controls: <<list the keys used to control the game>>")
-            safe_addstr(stdscr, 22, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
-            safe_addstr(stdscr, 23, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
+            if (game_element_height_offset < side_bar_lowest + 2):
+                game_element_height_offset = side_bar_lowest + 2
+            safe_addstr(stdscr, game_element_height_offset, 0, "Game Controls: <<list the keys used to control the game>>")
+            safe_addstr(stdscr, game_element_height_offset + 1, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
+            safe_addstr(stdscr, game_element_height_offset + 2, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
             stdscr.refresh()
 
             user_input = self._get_user_input(stdscr)
@@ -111,10 +110,6 @@ class GameUI:
         safe_addstr_colour(stdscr, 1, 0, "Thanks for playing", curses.color_pair(2))
 
     def _splash_player_info(self, stdscr):
-        # TODOFIXME: Update this
-        self._lowest_str = 10
-        # TODOFIXME: Update this
-        self._rightest_str = 5
         safe_addstr(stdscr, 4, 0, "Player name:")
         safe_addstr(stdscr, 5, 0, "Games played:")
         safe_addstr(stdscr, 6, 0, "Treasure high score:")
