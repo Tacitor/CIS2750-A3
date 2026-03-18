af94cd72311661c6dc349d12c684d976fc12b192
Tacitor <lukasjkrampitz@gmail.com>
2026-03-18 18:00:50 +0000

    Prepare to move off hardcoded curses positions. Add safe wrappers and catch if the terminal is too small. Don't count window resizing as a keypress.

--- python/treasure_runner/ui/game_ui.py
+++ python/treasure_runner/ui/game_ui.py
@@ -11,10 +11,16 @@ class GameUI:
         self._eng = GameEngine(self._config)
 
         self._message_bar = "Start by moving around"
+        self._lowest_str = 0
+        self._rightest_str = 0
 
     def launch(self):
         print("Launching the TUI...")
-        curses.wrapper(self._run_tui)
+        try:
+            curses.wrapper(self._run_tui)
+        except BufferError as exc:
+            print("[ERROR] Sorry it looks like your terminal is too small to display the game.")
+            print(str(exc))
         print("Done game and done with the GameEngine")
         self._eng.destroy()
         del self._eng
@@ -22,7 +28,6 @@ class GameUI:
     def _run_tui(self, stdscr):
         curses.curs_set(0)
         stdscr.clear() #'stdscr' is the main window
-        stdscr.refresh()
         curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_MAGENTA)
         curses.init_pair(2, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
 
@@ -39,27 +44,33 @@ class GameUI:
 
         user_input = ord('z')
         while user_input != ord('q'):
+            # TODOFIXME: Update this
+            self._lowest_str = 23
+            # TODOFIXME: Update this
+            self._rightest_str = game_element_offset
+
             stdscr.clear()
-            stdscr.addstr(0, 0, self._message_bar)
-            stdscr.addstr(1, 0, "<<room number and name here>>")
+            safe_addstr(stdscr, 0, 0, self._message_bar)
+            safe_addstr(stdscr, 1, 0, "<<room number and name here>>")
 
             room_str = self._eng.render_current_room()
             room_row = 3
             room_col = 0
             player_x, player_y = self._eng.player.get_position()
-            stdscr.addstr(room_row, room_col, room_str)
+            safe_addstr(stdscr, room_row, room_col, room_str)
+            #TODOFIXME: Make safe?
             stdscr.addch(room_row + player_y, room_col + player_x, "@", curses.color_pair(2))
 
-            stdscr.addstr(3, game_element_offset, "Game Elements:")
-            stdscr.addstr(5, game_element_offset, "@ - Julia")
-            stdscr.addstr(6, game_element_offset, "# - wall")
-            stdscr.addstr(7, game_element_offset, "$ - gold")
-            stdscr.addstr(8, game_element_offset, "X - portal")
-            stdscr.addstr(9, game_element_offset, "<<other elements/names here>>")
+            safe_addstr(stdscr, 3, game_element_offset, "Game Elements:")
+            safe_addstr(stdscr, 5, game_element_offset, "@ - Julia")
+            safe_addstr(stdscr, 6, game_element_offset, "# - wall")
+            safe_addstr(stdscr, 7, game_element_offset, "$ - gold")
+            safe_addstr(stdscr, 8, game_element_offset, "X - portal")
+            safe_addstr(stdscr, 9, game_element_offset, "<<other elements/names here>>")
 
-            stdscr.addstr(20, 0, "Game Controls: <<list the keys used to control the game>>")
-            stdscr.addstr(22, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
-            stdscr.addstr(23, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
+            safe_addstr(stdscr, 20, 0, "Game Controls: <<list the keys used to control the game>>")
+            safe_addstr(stdscr, 22, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
+            safe_addstr(stdscr, 23, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
             stdscr.refresh()
 
             user_input = self._get_user_input(stdscr)
@@ -78,6 +89,8 @@ class GameUI:
                 self._eng.move_player(Direction.EAST)
 
             self._message_bar = ""
+            # TODOFIXME: Remove this
+            self._message_bar = str(user_input)
         except ImpassableError:
             self._message_bar = "You can't go that way!"
         except GameEngineError:
@@ -87,21 +100,41 @@ class GameUI:
 
     def _splash_startup(self, stdscr):
         stdscr.clear()
-        stdscr.addstr(0, 0, "Julia's Jubilant Journey", curses.color_pair(1))
-        stdscr.addstr(1, 0, "is a Sokoban-style puzzle game.", curses.color_pair(2))
-        stdscr.addstr(2, 0, "Take control of Julia as she explores a world of mazes on her search for treasures.", curses.color_pair(2))
+        safe_addstr_colour(stdscr, 0, 0, "Julia's Jubilant Journey", curses.color_pair(1))
+        safe_addstr_colour(stdscr, 1, 0, "is a Sokoban-style puzzle game.", curses.color_pair(2))
+        safe_addstr_colour(stdscr, 2, 0, "Take control of Julia as she explores a world of mazes on her search for treasures.", curses.color_pair(2))
 
     def _splash_end(self , stdscr):
         stdscr.clear()
-        stdscr.addstr(0, 0, "Game over", curses.color_pair(1))
-        stdscr.addstr(1, 0, "Thanks for playing", curses.color_pair(2))
+        safe_addstr_colour(stdscr, 0, 0, "Game over", curses.color_pair(1))
+        safe_addstr_colour(stdscr, 1, 0, "Thanks for playing", curses.color_pair(2))
 
     def _splash_player_info(self, stdscr):
-        stdscr.addstr(4, 0, "Player name:")
-        stdscr.addstr(5, 0, "Games played:")
-        stdscr.addstr(6, 0, "Treasure high score:")
-        stdscr.addstr(7, 0, "Biggest world completed:")
-        stdscr.addstr(8, 0, "Last played:")
-        stdscr.addstr(10, 0, "Press any key to continue...")
+        # TODOFIXME: Update this
+        self._lowest_str = 10
+        # TODOFIXME: Update this
+        self._rightest_str = 5
+        safe_addstr(stdscr, 4, 0, "Player name:")
+        safe_addstr(stdscr, 5, 0, "Games played:")
+        safe_addstr(stdscr, 6, 0, "Treasure high score:")
+        safe_addstr(stdscr, 7, 0, "Biggest world completed:")
+        safe_addstr(stdscr, 8, 0, "Last played:")
+        safe_addstr(stdscr, 10, 5, "Press any key to continue...")
         stdscr.refresh() # Screen is redrawn — not printed
-        stdscr.getch() # Waits for a single key press (no Enter required)
+
+        char = stdscr.getch() # Waits for a single key press (no Enter required)
+        # To improve UX don't count window resizing as a keypress, all other are fine
+        while char == 410:
+            char = stdscr.getch()
+
+def safe_addstr_colour(stdscr, row, col, string, colour_pair):
+    try:
+        stdscr.addstr(row, col, string, colour_pair)
+    except curses.error as exc:
+        raise BufferError(str(exc)) from exc
+
+def safe_addstr(stdscr, row, col, string):
+    try:
+        stdscr.addstr(row, col, string)
+    except curses.error as exc:
+        raise BufferError(str(exc)) from exc
