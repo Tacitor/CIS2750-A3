5684754d66658ef561251b55b7617f8801796d4c
Tacitor <lukasjkrampitz@gmail.com>
2026-03-18 01:13:28 +0000

    Add a GE to the UI, now can play the game. Also bring back colour to the player char.

--- python/treasure_runner/ui/game_ui.py
+++ python/treasure_runner/ui/game_ui.py
@@ -1,16 +1,22 @@
 #GameUI should query the GameEngine for state and call rendering functions only.
 import curses
+from treasure_runner.models.game_engine import GameEngine
+from treasure_runner.bindings import Direction
 
 class GameUI:
     def __init__(self, config_path: str, profile_path: str):
         self._config = config_path
         self._profile = profile_path
+        self._eng = GameEngine(self._config);
 
         print("UI Time: " + self._profile + ", " + self._config)
 
     def launch(self):
         print("Launching the TUI...")
         curses.wrapper(self._run_tui)
+        print("Done game and done with the GameEngine")
+        self._eng.destroy()
+        del self._eng
 
     def _run_tui(self, stdscr):
         curses.curs_set(0)
@@ -28,26 +34,43 @@ class GameUI:
         self._splash_player_info(stdscr)
 
     def _ui_main_game(self, stdscr):
-        stdscr.clear()
-        stdscr.addstr(0, 0, "<<message bar here>>")
-        stdscr.addstr(1, 0, "<<room number and name here>>")
-
-        room_expected = "###################\n#..........#......#\n#$..#......#......#\n#...#......#......X\n#.#........#...##.#\n@...##....##..#####\n#...#......#...#..#\n#...#......#...$..#\n#...#......#......#\nX..$.......#......#\n#..$#......#......#\n#.................X\n###################\n"
         game_element_offset = 30
-        stdscr.addstr(3, 0, room_expected)
+        
+        user_input = ord('z')
+        while user_input != ord('q'):
+            stdscr.clear()
+            stdscr.addstr(0, 0, "<<message bar here>>")
+            stdscr.addstr(1, 0, "<<room number and name here>>")
+        
+            room_str = self._eng.render_current_room()
+            room_row = 3
+            room_col = 0
+            player_x, player_y = self._eng.player.get_position();
+            stdscr.addstr(room_row, room_col, room_str)
+            stdscr.addch(room_row + player_y, room_col + player_x, "@", curses.color_pair(2))
+            
+            stdscr.addstr(3, game_element_offset, "Game Elements:")
+            stdscr.addstr(5, game_element_offset, "@ - Julia")
+            stdscr.addstr(6, game_element_offset, "# - wall")
+            stdscr.addstr(7, game_element_offset, "$ - gold")
+            stdscr.addstr(8, game_element_offset, "X - portal")
+            stdscr.addstr(9, game_element_offset, "<<other elements/names here>>")
+            
+            stdscr.addstr(20, 0, "Game Controls: <<list the keys used to control the game>>")
+            stdscr.addstr(22, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
+            stdscr.addstr(23, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
+            stdscr.refresh()
 
-        stdscr.addstr(3, game_element_offset, "Game Elements:")
-        stdscr.addstr(5, game_element_offset, "@ - Julia")
-        stdscr.addstr(6, game_element_offset, "# - wall")
-        stdscr.addstr(7, game_element_offset, "$ - gold")
-        stdscr.addstr(8, game_element_offset, "X - portal")
-        stdscr.addstr(9, game_element_offset, "<<other elements/names here>>")
-
-        stdscr.addstr(20, 0, "Game Controls: <<list the keys used to control the game>>")
-        stdscr.addstr(22, 0, "<< Player Status bar here (e.g. gold collected, rooms played, rooms left>>")
-        stdscr.addstr(23, 0, "<<A title for your game here>>\t\t\t<<your email address here>>")
-        stdscr.refresh()
-        stdscr.getch()
+            user_input = stdscr.getch()
+            
+            if user_input == ord('w'):
+                self._eng.move_player(Direction.NORTH)
+            elif user_input == ord('a'):
+                self._eng.move_player(Direction.WEST)
+            elif user_input == ord('s'):
+                self._eng.move_player(Direction.SOUTH)
+            elif user_input == ord('d'):
+                self._eng.move_player(Direction.EAST)
 
     def _splash_startup(self, stdscr):
         stdscr.clear()
